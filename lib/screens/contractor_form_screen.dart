504402752dd387d0b052b4cddb8919dba2c94063
Keith Kariru Mutuku <karirukeith@students.uonbi.ac.ke>
2023-06-14 16:40:35 +0300

    Add contractor and marshall form screen

---
+++ lib/screens/contractor_form_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ContractorForms extends StatelessWidget {
+  const ContractorForms({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Marshall Forms"),
+      ),
+    );
+  }
+}
